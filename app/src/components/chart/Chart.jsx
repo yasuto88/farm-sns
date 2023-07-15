991617bb20040464dc9ac7797815d63ebc8c19df
yasuto88 <yasuto010788@gmail.com>
2023-07-15 16:48:22 +0900

    cart

---
+++ app/src/components/chart/Chart.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { collection, getDocs } from 'firebase/firestore';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function Chart() {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "This year",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "round",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "square",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#eee",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 1,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [4, 6, 16, 24, 36, 45, 53],
+      },
+      {
+        label: "Last year",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(255, 99, 132,0.4)",
+        borderColor: "rgba(255, 99, 132,1)",
+        borderCapStyle: "round",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "square",
+        pointBorderColor: "rgba(255, 99, 132,1)",
+        pointBackgroundColor: "#eee",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(255, 99, 132,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 1,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [3, 10, 21, 31, 34, 40, 48],
+      },
+    ],
+  };
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  );
+}
